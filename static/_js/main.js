d00cf0198f73bd1df818e28eb15a8f343f442acb
peitianx <peitian.xiong@berkeley.edu>
2016-12-05 16:03:22 -0800

    update search btn frontend

--- static/_js/main.js
+++ static/_js/main.js
@@ -1,10 +1,12 @@
 $("#tv_show").click(function(){
     $("#mainform").attr("action", "/tunefind_get_show_seasons");
-    $("#tv_show").css('color','red !important');
-	$("#tv_show").css('background-color','blue !important');
+	$(this).css('background-color', '#ff6f00');
+	$("#movie").css('background-color', '#e57373');
 });
 
 $("#movie").click(function(){
     $("#mainform").attr("action", "/tunefind_get_movie_songs");
+    $(this).css('background-color', '#ff5252');
+    $("#tv_show").css('background-color', '#ffa000');
 });
 
